26856bd0d0b8ea26ec447f9a00861fbaa856e3d4
Chris Gaona <thebeloved88@sbcglobal.net>
2021-04-25 15:14:37 -0700

    Builds out new detail view header section

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class RestaurantDetailViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var headerView: RestaurantDetailHeaderView!
+    
     var restaurant: Restaurant = Restaurant()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        // Configure header view
+        headerView.nameLabel.text = restaurant.name
+        headerView.typeLabel.text = restaurant.type
+        headerView.headerImageView.image = UIImage(named: restaurant.image)
+        
+        let heartImage = restaurant.isFavorite ? "heart.fill" : "heart"
+        headerView.heartButton.tintColor = restaurant.isFavorite ? .systemYellow : .white
+        headerView.heartButton.setImage(UIImage(systemName: heartImage), for: .normal)
     }
 }
---
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by Chris Gaona on 4/25/21.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+    
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var heartButton: UIButton!
+    
+}
