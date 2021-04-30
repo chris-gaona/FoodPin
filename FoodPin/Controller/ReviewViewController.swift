95d3b11414c14b99b45bf338a40d68fa44c28ba5
Chris Gaona <thebeloved88@sbcglobal.net>
2021-04-30 07:58:38 -0700

    Adds animations to ReviewViewController buttons

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 class ReviewViewController: UIViewController {
     
     @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var rateButtons: [UIButton]!
     
     var restaurant = Restaurant()
 
+    // This is called when the view is first loaded but not displayed on the screen yet
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,5 +26,52 @@ class ReviewViewController: UIViewController {
         // Makes the blue effect the entire view bounds
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
+        
+        let moveRightTransform = CGAffineTransform.init(translationX: 600, y: 0)
+        
+        // Make the buttons invisible
+        for rateButton in rateButtons {
+            rateButton.alpha = 0
+            rateButton.transform = moveRightTransform
+        }
+    }
+    
+    // This is called after viewDidLoad when the view is about to appear on the screen
+    override func viewWillAppear(_ animated: Bool) {
+//        // Adds fade in effect to the buttons - all the buttons fade in at the same time
+//        UIView.animate(withDuration: 2.0, animations: {
+//            self.rateButtons[0].alpha = 1.0
+//            self.rateButtons[1].alpha = 1.0
+//            self.rateButtons[2].alpha = 1.0
+//            self.rateButtons[3].alpha = 1.0
+//            self.rateButtons[4].alpha = 1.0
+//        })
+        
+        // Adds fade in effect to buttons - each buttons fades in slightly after the other
+        UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
+            self.rateButtons[0].alpha = 1.0
+            // Setting the button's transform back to .identity resets it to its original position
+            self.rateButtons[0].transform = .identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
+            self.rateButtons[1].alpha = 1.0
+            self.rateButtons[1].transform = .identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
+            self.rateButtons[2].alpha = 1.0
+            self.rateButtons[2].transform = .identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
+            self.rateButtons[3].alpha = 1.0
+            self.rateButtons[3].transform = .identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
+            self.rateButtons[4].alpha = 1.0
+            self.rateButtons[4].transform = .identity
+        }, completion: nil)
     }
 }
