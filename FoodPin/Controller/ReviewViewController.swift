38370342a0d60a235059a18f06a77383c63cdefc
Chris Gaona <thebeloved88@sbcglobal.net>
2021-04-29 19:58:36 -0700

    Adds blur effect to the image background on ReviewViewController

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -17,5 +17,12 @@ class ReviewViewController: UIViewController {
         super.viewDidLoad()
 
         backgroundImageView.image = UIImage(named: restaurant.image)
+        
+        // Applying a blur effect
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        // Makes the blue effect the entire view bounds
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
     }
 }
